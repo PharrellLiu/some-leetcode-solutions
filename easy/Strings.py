440c71d331ccd4a1c188d3e42c0f24d7b8de5369
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-06-05 14:51:01 +0800

    update

--- easy/Strings.py
+++ easy/Strings.py
@@ -31,3 +31,64 @@ def reverse(self, x: int) -> int:
         return ans
     else:
         return 0
+
+
+# First Unique Character in a String
+def firstUniqChar(self, s: str) -> int:
+    dic = {}
+    i = 0
+    while i < len(s):
+        if s[i] in dic:
+            dic[s[i]] = -1
+        else:
+            dic[s[i]] = i
+        i += 1
+    ans = -1
+    for j in dic:
+        if dic[j] != -1:
+            if ans == -1:
+                ans = dic[j]
+            else:
+                ans = min(ans, dic[j])
+    return ans
+
+
+# Valid Anagram
+def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    dic = {}
+    i = 0
+    while i < len(s):
+        if s[i] in dic:
+            dic[s[i]] += 1
+        else:
+            dic[s[i]] = 1
+        if t[i] in dic:
+            dic[t[i]] -= 1
+        else:
+            dic[t[i]] = -1
+        i += 1
+    for j in dic:
+        if dic[j] != 0:
+            return False
+    return True
+
+
+# Valid Palindrome
+def isPalindrome(self, s: str) -> bool:
+    s = s.lower()
+    i = 0
+    while i < len(s):
+        if 47 < ord(s[i]) < 58 or 96 < ord(s[i]) < 123:
+            i += 1
+        else:
+            s = s[:i] + s[i + 1:]
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
