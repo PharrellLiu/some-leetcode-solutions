2ff7f4ac3cfa713fedf6b518fe33d98ed6effce6
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-06-05 20:20:41 +0800

    Update Linked List.py

--- easy/Linked List.py
+++ easy/Linked List.py
@@ -45,3 +45,80 @@ def reverseList(self, head: ListNode) -> ListNode:
             return prev
         else:
             furt = curr.next
+
+
+# Merge Two Sorted Lists
+def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    if l1 is None and l2 is None:
+        return None
+    head = ListNode(0)
+    curr = head
+    while l1 is not None or l2 is not None:
+        if l1 is not None and l2 is not None:
+            if l1.val <= l2.val:
+                curr.val = l1.val
+                l1 = l1.next
+            else:
+                curr.val = l2.val
+                l2 = l2.next
+            curr.next = ListNode(0)
+            curr = curr.next
+        elif l1 is None and l2 is not None:
+            curr.val = l2.val
+            curr.next = l2.next
+            break
+        elif l1 is not None and l2 is None:
+            curr.val = l1.val
+            curr.next = l1.next
+            break
+    return head
+
+
+# Palindrome Linked List
+def isPalindrome(self, head: ListNode) -> bool:
+    if head is None:
+        return True
+    if head.next is None:
+        return True
+    slow = head
+    fast = head
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+    if fast is not None:
+        slow = slow.next
+
+    def reverse(root):
+        prev = None
+        curr = root
+        furt = curr.next
+        while True:
+            curr.next = prev
+            prev = curr
+            curr = furt
+            if curr is None:
+                return prev
+            else:
+                furt = curr.next
+
+    newHead = reverse(slow)
+    while newHead is not None:
+        if newHead.val != head.val:
+            return False
+        newHead = newHead.next
+        head = head.next
+    return True
+
+
+# Linked List Cycle
+def hasCycle(self, head: ListNode) -> bool:
+    if head is None:
+        return None
+    slow = head
+    fast = head.next
+    while slow != fast:
+        if fast is None or fast.next is None:
+            return False
+        fast = fast.next.next
+        slow = slow.next
+    return True
