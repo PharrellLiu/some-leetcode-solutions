6fdd513a31553fdd32b86793faa2c686f8084943
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-06-16 16:17:25 +0800

    Update Dynamic Programming.py

--- medium/Dynamic Programming.py
+++ medium/Dynamic Programming.py
@@ -15,3 +15,18 @@ def canJump(self, nums: List[int]) -> bool:
             return True
         i += 1
     return True
+
+
+# Unique Paths
+def uniquePaths(self, m: int, n: int) -> int:
+    dp = [[0 for _ in range(m)] for _ in range(n)]
+    dp[0][0] = 1
+    for i in range(len(dp)):
+        for j in range(len(dp[i])):
+            if i - 1 >= 0 and j - 1 < 0:
+                dp[i][j] = dp[i - 1][j]
+            if j - 1 >= 0 and i - 1 < 0:
+                dp[i][j] = dp[i][j - 1]
+            if i - 1 >= 0 and j - 1 >= 0:
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+    return dp[-1][-1]
