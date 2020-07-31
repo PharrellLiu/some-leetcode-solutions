6a8014687202b510b18baf5858730f39c3fd1c52
XINYUAN LIU <53179832+PharrellLiu@users.noreply.github.com>
2020-07-31 10:07:12 +0800

    Update Strings.py

--- easy/Strings.py
+++ easy/Strings.py
@@ -135,7 +135,7 @@ def strStr(self, haystack: str, needle: str) -> int:
         return 0
     i = 0
     while i < len(haystack) - len(needle) + 1:
-        while i < len(haystack) and haystack[i] != needle[0]:
+        while i < len(haystack) - len(needle) + 1 and haystack[i] != needle[0]:
             i += 1
         if haystack[i:i + len(needle)] == needle:
             return i
